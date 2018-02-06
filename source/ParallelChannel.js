1d9f167db7da169294cae431f29a3f055ee48788
Perry Mitchell <perry@perrymitchell.net>
2018-02-06 21:52:05 +0200

    Add new parallel channel

---
+++ source/ParallelChannel.js
@@ -0,0 +1,53 @@
+const Channel = require("./Channel.js");
+
+class ParallelChannel extends Channel {
+
+    constructor(...args) {
+        super(...args);
+        this._parallelism = 2;
+        this._runningTasks = [];
+        this.canRunAcrossTaskTypes = false;
+    }
+
+    get isEmpty() {
+        return super.isEmpty && this._runningTasks.length <= 0;
+    }
+
+    get parallelism() {
+        return this._parallelism;
+    }
+
+    get runningTasks() {
+        return this._runningTasks;
+    }
+
+    set parallelism(count) {
+        const newCount = Math.max(count, 1);
+        this._parallelism = newCount;
+    }
+
+    _runNextItem() {
+        let itemsToRun = this.parallelism - this.runningTasks.length;
+        if (itemsToRun <= 0) {
+            return;
+        }
+        if (this.runningTasks.length === 0 && this.tasks.length === 0) {
+            this.isRunning = false;
+            this.emit("stopped");
+        }
+        while (itemsToRun > 0) {
+            itemsToRun -= 1;
+            const item = this.retrieveNextItem();
+            this.runningTasks.push(item);
+            item
+                .execute()
+                .then(() => {
+                    this.runningTasks.splice(this.runningTasks.indexOf(item), 1);
+                    this._runNextItem();
+                });
+        }
+    }
+
+}
+
+module.exports = ParallelChannel;
